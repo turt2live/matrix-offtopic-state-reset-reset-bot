a8cf644022ad69bb2b7f16d07a622e7e74b99506
Travis Ralston <travpc@gmail.com>
2018-01-21 22:34:16 -0700

    Set up the log service correctly

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const LogService = require("matrix-js-snippets").LogService;
 const config = require("config");
+
+LogService.configure(config["logging"]);
+
 const sdk = require("matrix-js-sdk");
 const CommandHandler = require("./src/CommandHandler");
 const UnbanHandler = require("./src/UnbanHandler");
 const matrixUtils = require("matrix-js-snippets");
-const LogService = require("matrix-js-snippets").LogService
-
-LogService.configure(config["logging"]);
 
 const client = sdk.createClient({
     baseUrl: config['homeserverUrl'],
